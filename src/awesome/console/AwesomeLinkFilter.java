ff3f98e9df9299b37739a4c0c777ccef6d53834a
Tim Segbert <tim.segbert@gmx.de>
2015-01-28 17:31:30 +0100

    Searching files recursively.

--- src/awesome/console/AwesomeLinkFilter.java
+++ src/awesome/console/AwesomeLinkFilter.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
+import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,21 +23,21 @@ public class AwesomeLinkFilter implements Filter {
 
 	@Override
 	public Result applyFilter(String s, int endPoint) {
-		System.err.println("Checking string: " + s);
 		int startPoint = endPoint - s.length();
 		Matcher matcher = URL_PATTERN.matcher(s);
 		if (matcher.find()) {
-			System.err.println("Found url: " + matcher.group(1));
 			return new Result(startPoint + matcher.start(),
 					startPoint + matcher.end(), new OpenUrlHyperlinkInfo(matcher.group(1)));
 		} else {
 			matcher = FILE_PATTERN.matcher(s);
 			if (matcher.find()) {
 
-				VirtualFile file = project.getBaseDir().getFileSystem().findFileByPath(project.getBasePath() + "/src/" + matcher.group(1));
+				final File file1 = findFile(matcher.group(1), new File(project.getBasePath()));
+				if (file1 == null) {
+					return null;
+				}
+				VirtualFile file = project.getBaseDir().getFileSystem().findFileByPath(file1.getPath());
 				if (file != null) {
-					System.err.println("I think I found something! -> " + file.toString());
-
 					OpenFileDescriptor fileDescriptor = new OpenFileDescriptor(project,
 							file,
 							matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3)) - 1, // line
@@ -49,5 +50,20 @@ public class AwesomeLinkFilter implements Filter {
 			}
 		}
 		return new Result(startPoint, endPoint, null, new TextAttributes());
+	}
+
+	private File findFile(final String name, final File dir) {
+		final File[] files = dir.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				final File file1 = findFile(name, file);
+				if (file1 != null) {
+					return file1;
+				}
+			} else if (file.getName().contains(name)) {
+				return file;
+			}
+		}
+		return null;
 	}
 }
