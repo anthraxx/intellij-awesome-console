219cd629d31b0ab6b313dc5502a98f1ae4c9a82e
anthraxx <levente@leventepolyak.net>
2015-01-28 18:50:30 +0100

    implemented multiple file link support

--- src/awesome/console/AwesomeLinkFilter.java
+++ src/awesome/console/AwesomeLinkFilter.java
@@ -1,10 +1,10 @@
 package awesome.console;
 
 import com.intellij.execution.filters.Filter;
-import com.intellij.execution.filters.OpenFileHyperlinkInfo;
+import com.intellij.execution.filters.HyperlinkInfo;
+import com.intellij.execution.filters.HyperlinkInfoFactory;
 import com.intellij.ide.browsers.OpenUrlHyperlinkInfo;
 import com.intellij.openapi.editor.markup.TextAttributes;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
@@ -16,21 +16,15 @@ import java.util.regex.Pattern;
 
 public class AwesomeLinkFilter implements Filter {
 	private static final Pattern FILE_PATTERN = Pattern.compile("([a-zA-Z][a-zA-Z0-9/\\-_\\.]+\\.[a-z]+)(:(\\d+))?(:(\\d+))?");
-	private static final Pattern URL_PATTERN = Pattern.compile("((file)?(https?)?://[-_.!~*\\\\'()a-zA-Z0-9;\\\\/?:\\\\@&=+\\\\$,%#]+)");
+	private static final Pattern URL_PATTERN = Pattern.compile("((ftp)?(file)?(https?)?://[-_.!~*\\\\'()a-zA-Z0-9;\\\\/?:\\\\@&=+\\\\$,%#]+)");
 	private final Project project;
 
 	public AwesomeLinkFilter(Project project) {
 		this.project = project;
 	}
 
-	public AwesomeLinkFilter() {
-		this.project = null;
-	}
-
 	@Override
 	public Result applyFilter(String s, int endPoint) {
-		final List<ResultItem> results = new ArrayList<ResultItem>();
-
 		int startPoint = endPoint - s.length();
 		Matcher matcher = URL_PATTERN.matcher(s);
 
@@ -41,30 +35,22 @@ public class AwesomeLinkFilter implements Filter {
 		matcher = FILE_PATTERN.matcher(s);
 		if (matcher.find()) {
 			final List<File> matchingFiles = new ArrayList<File>();
-			final List <VirtualFile> matchingVirtualFiles = new ArrayList<VirtualFile>();
+			final List <VirtualFile> virtualFiles = new ArrayList<VirtualFile>();
 			findFile(matchingFiles, matcher.group(1), new File(project.getBasePath()));
 
-			Result result = null;
 			for (final File file : matchingFiles) {
 				VirtualFile virtualFile = project.getBaseDir().getFileSystem().findFileByPath(file.getPath());
 				if (virtualFile == null) {
 					continue;
 				}
-				matchingVirtualFiles.add(virtualFile);
-				OpenFileDescriptor fileDescriptor = new OpenFileDescriptor(project,
-						virtualFile,
-						matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3)) - 1, // line
-						matcher.group(5) == null ? 0 : Integer.parseInt(matcher.group(5)) - 1 // column
-				);
-				result = new Result(startPoint + matcher.start(),
-						startPoint + matcher.end(), new OpenFileHyperlinkInfo(fileDescriptor));
-				result.setNextAction(NextAction.CONTINUE_FILTERING);
-				results.add(result);
+				virtualFiles.add(virtualFile);
 			}
-			if (null != result) {
-				result.setNextAction(NextAction.EXIT);
-			}
-			return new Result(results);
+			HyperlinkInfo linkInfo = HyperlinkInfoFactory.getInstance().createMultipleFilesHyperlinkInfo(
+					virtualFiles,
+					matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3)) - 1,
+					project
+			);
+			return new Result(startPoint + matcher.start(), startPoint + matcher.end(), linkInfo);
 		}
 		return new Result(startPoint, endPoint, null, new TextAttributes());
 	}
