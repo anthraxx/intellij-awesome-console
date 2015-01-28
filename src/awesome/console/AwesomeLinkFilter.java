6e9d94c2302666f5792df42483ca181aaa195f31
anthraxx <levente@leventepolyak.net>
2015-01-28 19:39:00 +0100

    implemented file search caching

--- src/awesome/console/AwesomeLinkFilter.java
+++ src/awesome/console/AwesomeLinkFilter.java
@@ -8,14 +8,14 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AwesomeLinkFilter implements Filter {
 	private static final Pattern FILE_PATTERN = Pattern.compile("([a-zA-Z][a-zA-Z0-9/\\-_\\.]+\\.[a-z]+)(:(\\d+))?(:(\\d+))?");
 	private static final Pattern URL_PATTERN = Pattern.compile("((ftp)?(file)?(https?)?://[-_.!~*\\\\'()a-zA-Z0-9;\\\\/?:\\\\@&=+\\\\$,%#]+)");
+	private final Map<String, List<File>> fileCache = new HashMap<String, List<File>>();
 	private final Project project;
 
 	public AwesomeLinkFilter(Project project) {
@@ -48,11 +48,15 @@ public class AwesomeLinkFilter implements Filter {
 	public List<ResultItem> getResultItemsFile(final String line, final int startPoint) {
 		final List<ResultItem> results = new ArrayList<ResultItem>();
 		final Matcher matcher = FILE_PATTERN.matcher(line);
+		if (fileCache.isEmpty()) {
+			createFileCache(new File(project.getBasePath()));
+		}
 		while (matcher.find()) {
-			final List<File> matchingFiles = new ArrayList<File>();
 			final List <VirtualFile> virtualFiles = new ArrayList<VirtualFile>();
-			findFile(matchingFiles, matcher.group(1), new File(project.getBasePath()));
-
+			final List<File> matchingFiles = fileCache.get(matcher.group(1));
+			if (null == matchingFiles) {
+				continue;
+			}
 			for (final File file : matchingFiles) {
 				final VirtualFile virtualFile = project.getBaseDir().getFileSystem().findFileByPath(file.getPath());
 				if (virtualFile == null) {
@@ -78,17 +82,17 @@ public class AwesomeLinkFilter implements Filter {
 		return results;
 	}
 
-	public List<File> findFile(final List<File> matches, final String name, final File dir) {
+	private void createFileCache(final File dir) {
 		final File[] files = dir.listFiles();
 		for (final File file : files) {
 			if (file.isDirectory()) {
-				findFile(matches, name, file);
+				createFileCache(file);
 				continue;
 			}
-			if (file.getName().equals(name)) {
-				matches.add(file);
+			if (!fileCache.containsKey(file.getName())) {
+				fileCache.put(file.getName(), new ArrayList<File>());
 			}
+			fileCache.get(file.getName()).add(file);
 		}
-		return matches;
 	}
 }
