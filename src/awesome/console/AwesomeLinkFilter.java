575f389082310e6866da2c6c1ecf270dffae4aa9
anthraxx <levente@leventepolyak.net>
2015-01-28 18:30:00 +0100

    try to support multiple file results

--- src/awesome/console/AwesomeLinkFilter.java
+++ src/awesome/console/AwesomeLinkFilter.java
@@ -9,6 +9,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,49 +23,61 @@ public class AwesomeLinkFilter implements Filter {
 		this.project = project;
 	}
 
+	public AwesomeLinkFilter() {
+		this.project = null;
+	}
+
 	@Override
 	public Result applyFilter(String s, int endPoint) {
+		final List<ResultItem> results = new ArrayList<ResultItem>();
+
 		int startPoint = endPoint - s.length();
 		Matcher matcher = URL_PATTERN.matcher(s);
+
 		if (matcher.find()) {
 			return new Result(startPoint + matcher.start(),
 					startPoint + matcher.end(), new OpenUrlHyperlinkInfo(matcher.group(1)));
-		} else {
-			matcher = FILE_PATTERN.matcher(s);
-			if (matcher.find()) {
+		}
+		matcher = FILE_PATTERN.matcher(s);
+		if (matcher.find()) {
+			final List<File> matchingFiles = new ArrayList<File>();
+			final List <VirtualFile> matchingVirtualFiles = new ArrayList<VirtualFile>();
+			findFile(matchingFiles, matcher.group(1), new File(project.getBasePath()));
 
-				final File file1 = findFile(matcher.group(1), new File(project.getBasePath()));
-				if (file1 == null) {
-					return null;
+			Result result = null;
+			for (final File file : matchingFiles) {
+				VirtualFile virtualFile = project.getBaseDir().getFileSystem().findFileByPath(file.getPath());
+				if (virtualFile == null) {
+					continue;
 				}
-				VirtualFile file = project.getBaseDir().getFileSystem().findFileByPath(file1.getPath());
-				if (file != null) {
-					OpenFileDescriptor fileDescriptor = new OpenFileDescriptor(project,
-							file,
-							matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3)) - 1, // line
-							matcher.group(5) == null ? 0 : Integer.parseInt(matcher.group(5)) - 1 // column
-					);
-
-					return new Result(startPoint + matcher.start(),
-							startPoint + matcher.end(), new OpenFileHyperlinkInfo(fileDescriptor));
-				}
+				matchingVirtualFiles.add(virtualFile);
+				OpenFileDescriptor fileDescriptor = new OpenFileDescriptor(project,
+						virtualFile,
+						matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3)) - 1, // line
+						matcher.group(5) == null ? 0 : Integer.parseInt(matcher.group(5)) - 1 // column
+				);
+				result = new Result(startPoint + matcher.start(),
+						startPoint + matcher.end(), new OpenFileHyperlinkInfo(fileDescriptor));
+				result.setNextAction(NextAction.CONTINUE_FILTERING);
+				results.add(result);
 			}
+			if (null != result) {
+				result.setNextAction(NextAction.EXIT);
+			}
+			return new Result(results);
 		}
 		return new Result(startPoint, endPoint, null, new TextAttributes());
 	}
 
-	private File findFile(final String name, final File dir) {
+	private List<File> findFile(List<File> matches, final String name, final File dir) {
 		final File[] files = dir.listFiles();
 		for (File file : files) {
 			if (file.isDirectory()) {
-				final File file1 = findFile(name, file);
-				if (file1 != null) {
-					return file1;
-				}
+				findFile(matches, name, file);
 			} else if (file.getName().contains(name)) {
-				return file;
+				matches.add(file);
 			}
 		}
-		return null;
+		return matches;
 	}
 }
